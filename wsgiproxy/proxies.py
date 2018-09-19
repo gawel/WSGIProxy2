2aeef998e54e7bba040695e4ee650bb1bb33945b
jj <jj.weber@gmail.com>
2018-09-19 11:38:47 -0700

    Remove old code path

--- wsgiproxy/proxies.py
+++ wsgiproxy/proxies.py
@@ -67,7 +67,6 @@ class HttpClient(object):
         ConnClass = ssl and self.HTTPSConnection or self.HTTPConnection
         uri = ssl and uri[8:] or uri[7:]
         port = ssl and 443 or 80
-        host, path = uri.split('/', 1)
         try:
             host, path = uri.split('/', 1)
         except ValueError:
